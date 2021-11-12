3f16e3b62df6cb98c33622ab55d17071b03d8f68
Henry Gadea <henry_gadea@hotmail.com>
2021-11-12 12:34:52 -0600

    Create extension to initialize httpclient service

--- SuperHeroSearch_WebApp/Extensions/ServiceCollectionExtensions.cs
+++ SuperHeroSearch_WebApp/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,10 @@
-﻿using HGUtils.Helpers.Configuration;
+﻿using Flurl;
+using HGUtils.Helpers.Configuration;
 using Microsoft.Extensions.Configuration;
+using SuperHeroSearch_App.Contracts.HttpClients;
+using SuperHeroSearch_App.Services.HttpClients;
 using SuperHeroSearch_Common.Configurations;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -13,5 +17,15 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddIoC(this IServiceCollection services) =>
             services;
 
+        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration config)
+        {
+            var apiConfig = config.GetConfig<ApiConfig>() ?? throw new ArgumentNullException(nameof(ApiConfig));
+            var baseAddress = Url.Combine(apiConfig.BaseUrl, apiConfig.AccessToken, "/");
+
+            services
+                .AddHttpClient<ISuperHeroHttpClient, SuperHeroHttpClientService>(client => client.BaseAddress = new Uri(baseAddress));
+
+            return services;
+        }
     }
 }
