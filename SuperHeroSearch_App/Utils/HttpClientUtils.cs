ea8b3038094649888c0341f5b064f80227ec6f0d
Henry Gadea <henry_gadea@hotmail.com>
2021-11-12 12:32:57 -0600

    Create util extension for assign route parameters

---
+++ SuperHeroSearch_App/Utils/HttpClientUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SuperHeroSearch_App.Utils
+{
+    public static class HttpClientUtils
+    {
+        public static string AssignRouteParameters(this string url, params (string, string)[] parameters)
+        {
+            var lowerurl = url.ToLower();
+
+            foreach (var parameter in parameters)
+            {
+                (var parameterName, var parameterValue) = parameter;
+
+                var paramToFind = "{" + parameterName.ToLower() + "}";
+
+                lowerurl = lowerurl.Replace(paramToFind, parameterValue.ToLower());
+            }
+
+            if (lowerurl.Contains("{") || lowerurl.Contains("}")) throw new FormatException($"La url {lowerurl} contiene parametros sin asignar");
+
+            return lowerurl;
+        }
+    }
+}
